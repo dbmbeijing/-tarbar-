d50625a3c867341e7333e5c2e284044830dac643
User <user@userdemacbook-pro.local>
2015-11-08 18:29:15 +0800

    1.初始化项目

---
+++ 控制器之间的传值/控制器之间的传值/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  控制器之间的传值
+//
+//  Created by User on 15/11/8.
+//  Copyright © 2015年 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 控制器之间的传值/控制器之间的传值/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  控制器之间的传值
+//
+//  Created by User on 15/11/8.
+//  Copyright © 2015年 User. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
